68bb062fcc2cabbc25855a013f50dab084f3aa5f
Usman Qidwai <usmanqidwai21@gmail.com>
2023-08-23 21:27:29 -0400

    Functions Completed

---
+++ index.js
@@ -0,0 +1,6 @@
+function introductionWithLanguageOptional(name,language='JavaScript'){
+  console.log(`My name is ${name} and I speak ${language}`);
+
+}
+
+introductionWithLanguageOptional('Mark','JavaScript')
