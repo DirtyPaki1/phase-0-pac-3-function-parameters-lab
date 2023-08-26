904b85fc3d0df3230e243eaa28c2bea1b18d4876
Usman Qidwai <usmanqidwai21@gmail.com>
2023-08-26 19:02:52 -0400

    Function Parameters Completed

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-function introductionWithLanguageOptional(name,language='JavaScript'){
-  console.log(`My name is ${name} and I speak ${language}`);
-
+function introduction(name){
+  return `Hi, my name is ${name}.`
 }
 
 
-introductionWithLanguageOptional('Mark')
+
+function introductionWithLanguage(name,language){
+  return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
+
+function introductionWithLanguageOptional(name,language='JavaScript'){
+  return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
+
+
+function introductionWithLanguage(name,language){
+  return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
+
+
